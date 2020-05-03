00e0d81f335c0a215412b18832e264fb2f082f9e
Billy Tukija Richard <billy.tukija@onlineapp.com.br>
2020-05-03 00:33:45 -0300

    Exemple of Range use instead of Substring

---
+++ RangeInsteadOfSubstring/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RangeInsteadOfSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Je suis un bantu";
+
+            // Console.WriteLine(text.Substring(10,6));
+
+            // Note: Why not to use Range instead of Substring?
+
+            Console.WriteLine(text[10..16]);
+        }
+    }
+}
